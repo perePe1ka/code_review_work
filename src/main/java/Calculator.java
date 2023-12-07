1665f2d5920d2d9c5e455936e9555feaa5830770
perePe1ka <vladuss_1337@list.ru>
2023-12-07 17:50:45 +0300

    commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,33 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Нельзя с нулём");
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, char operator) {
+        if (operator == '+') {
+            return add(a, b);
+        } else if (operator == '-') {
+            return dif(a, b);
+        } else if (operator == '*') {
+            return times(a, b);
+        } else if (operator == '/') {
+            return div(a, b);
+        } else {
+            throw new IllegalArgumentException("Несуществующий оператор: " + operator);
+        }
     }
 }
